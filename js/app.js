35efd6bb4d5058f7936ca5078cd0bcbced2f4138
Tomas Ignacio Garcia <15tommgarcia@gmail.com>
2020-08-10 18:28:45 -0300

    Agrega cursos al carrito

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// Variables
+
+const carrito = document.querySelector('#carrito');
+const cursos = document.querySelector('#lista-cursos');
+const listaCursos = document.querySelector('#lista-carrito tbody');
+
+// Event Listeners
+
+eventListeners()
+
+function eventListeners() {
+    cursos.addEventListener('click', comprarCurso);
+}
+
+// Functions
+
+function comprarCurso(e) {
+    e.preventDefault();
+    // Delegation para agregar-carrito
+    if (e.target.classList.contains('agregar-carrito')) {
+        const curso = e.target.parentElement.parentElement;
+        // Leer curso seleccionado para enviar sus datos       
+        leerDatosCurso(curso);
+    };
+}
+
+function leerDatosCurso(curso) {
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        nombre: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+
+    }
+    insertarAlCarrito(infoCurso);
+}
+
+function insertarAlCarrito(curso) {    
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src='${curso.imagen}' width='100'>
+        </td>
+        <td>${curso.nombre}</td>
+        <td>${curso.precio}</td>    
+        <td>
+            <a href='#' class='borrar-curso' data-id='${curso.id}'>X</a>
+        </td>
+    `;
+    listaCursos.appendChild(row);
+}
